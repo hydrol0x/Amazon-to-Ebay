7300539dea47fb2b3b35a49f936e7b3648ee7064
hydrol0x <mrjacobry@gmail.com>
2022-06-24 14:35:50 -0400

    fixed list out of range error for handle_list

--- amzn_product_scraper.py
+++ amzn_product_scraper.py
@@ -10,13 +10,15 @@ url = str(input("Enter Amazon URL: "))
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
 # Scraping HTML
+print("Sending Page Request")
 page = requests.get(url, headers=headers) 
+print("Parsing page data")
 page = BeautifulSoup(page.content, "html.parser")
-
+print("Page data parsed")
 
 ### * * FUNCTIONS HANDLING DIFFERENT IDS * * ###
 
-# function handles table type of listing; th is the heading for the product info and td is the value.  
+# *function handles table type of listing; th is the heading for the product info and td is the value.  
 def handle_table(id):
     prod_info = (page.find(id=id).find_all('tr'))
     prod_info_dict = {}
@@ -24,8 +26,7 @@ def handle_table(id):
         prod_info_dict[(info.find('th').text.strip())] = (info.find('td').text.strip().replace('\u200e', '').replace('\n',''))
     return prod_info_dict
 
-# function handles the alternate table-listing where only td tags are used and no th tags are used 
-# TODO differentiate between first td tag and second for key/value, use <em> tags on heading
+# *function handles the alternate table-listing where only td tags are used and no th tags are used 
 def handle_alt_table(id):
     prod_info = (page.find(id=id).find_all('tr'))
     prod_info_dict = {}
@@ -34,49 +35,56 @@ def handle_alt_table(id):
         (prod_info_dict[info.contents[1].text.strip()]) = (info.contents[3].text.strip())
     return prod_info_dict
 
-# function handles list type listing (<ul>)
+# * function handles list type listing (<ul>)
 def handle_list(id):
-    prod_info = (page.find(id=id).find_all('li'))
+    prod_info = (page.find_all(id=id)[1].find_all('li'))
     prod_info_dict = {}
     for info in prod_info:
         prod_info_dict[(info.span.contents[1].text.replace(':','').replace('\n', '').replace('\u200f', '').replace('\u200e','').strip())] = (info.span.contents[3].text.strip())
     return prod_info_dict
+    
 
 
 # List of possible ID values containing product info
 ids = ['prodDetails', 'tech', 'detailBullets_feature_div']
 
-# TODO try except
 # scrape product information into a dictionary
-# prod details : tableli, tech: altli, detailBulletsWrapper_feature_div : listli <-- which function to use
 def get_info(ids=ids):
     for id in ids:
+        print(f"checking {id} ID")
         match id:
             case 'prodDetails':
                 try:
                     data = handle_table(id)
                 except AttributeError:
-                    pass
+                    print("ID not prodDetails")
                 else:
                     return data
             case 'tech':
                 try:
                     data = handle_alt_table(id)
                 except AttributeError:
-                    pass
+                    print("ID not tech")
                 else:
                     return data
             case 'detailBullets_feature_div':
                 try:
                     data = handle_list(id)
                 except AttributeError:
-                    pass
+                    print("ID not detailBullets_feature_div")
                 else:
                     return data
             case _:
                 return "Invalid product"
-       
 
-with open('product.txt', 'w') as json_file:
-  json.dump(get_info(ids), json_file)
-print(get_info(ids))
+# product information       
+prod_info_dict = get_info(ids)
+
+# Serializing JSON
+json_object = json.dumps(prod_info_dict, indent = 4)
+
+# Writing to JSON 
+with open("./product.json", "w") as outfile:
+    outfile.write(json_object)
+
+print("Finished writing to products.json")
